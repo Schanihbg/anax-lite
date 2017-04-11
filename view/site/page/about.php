42c2da4a770c6a65c67c26dc0a60b3de625f0521
Losningen <schani@live.se>
2017-04-11 15:24:03 +0200

    Added status to about page

--- view/site/page/about.php
+++ view/site/page/about.php
@@ -2,3 +2,4 @@
 <p>This site is for a course in Object-oriented programming PHP (OOPHP) at Blekinge Tekniska Högskola.</p>
 
 <a href="https://github.com/Schanihbg/anax-lite">A link to the github project</a>
+<a href="status">Status page in JSON.</a>
