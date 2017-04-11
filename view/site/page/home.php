90369767da0e6e0329a58fc3cb0a800158dee051
Losningen <schani@live.se>
2017-04-11 14:03:44 +0200

    Modified home page

--- view/site/page/home.php
+++ view/site/page/home.php
@@ -1,2 +1,24 @@
 <h1>Home</h1>
-<p>This is the homepage.</p>
+<p>Hej, Lösningen här!</p>
+
+<img src="image/me.png" class="right" alt="Bild på mig">
+
+<p>Föddes i Helsingborg. Flyttade min dator och diverse tillhörigheter till Karlskrona år 2012.</p>
+
+<p>
+    Jobbade ideellt i diverse sektioner inom Blekinge Studentkår mellan 2014 och 2016.<br />
+    Märkte att när jag jobbade som Sysadmin att jag trivs bättre med frontend än backend.
+</p>
+
+<p>
+    <b>Tidsplan i Karlskrona: </b><br />
+    12/13 - Tekniskt Basår <br />
+    13/14 - Spelprogrammering <br />
+    14/15 - Läste upp lite kurser från Spelprogrammering <br />
+    15/16 - Systemadministratör på Blekinge Studentkår <br />
+    <br />
+    <b>Framtida planer</b><br />
+    16/17 - Webbprogrammering <br />
+    17/18 - Webbprogrammering (Förhoppningsvis) <br />
+    18/19 - Webbprogrammering (Förhoppningsvis) <br />
+</p>
