917adec1ac4083d326345af2b989942e3360d96c
Losningen <schani@live.se>
2017-04-11 13:50:24 +0200

    Modified about page

--- view/site/page/about.php
+++ view/site/page/about.php
@@ -1,2 +1,4 @@
 <h1>About</h1>
-<p>This is the page about me.</p>
+<p>This site is for a course in Object-oriented programming PHP (OOPHP) at Blekinge Tekniska Högskola.</p>
+
+<a href="https://github.com/Schanihbg/anax-lite">A link to the github project</a>
