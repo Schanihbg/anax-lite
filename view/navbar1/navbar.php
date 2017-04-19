23bf30670548f5737ed52887c4312d2727457654
Losningen <schani@live.se>
2017-04-19 11:21:18 +0200

    Applied bootstrap to site

--- view/navbar1/navbar.php
+++ view/navbar1/navbar.php
@@ -1,8 +1,5 @@
 <?php
 $navbar = [
-    "config" => [
-        "navbar-class" => "navbar"
-    ],
     "items" => [
         "home" => [
             "title" => "Home",
@@ -19,9 +16,24 @@ $navbar = [
     ]
 ];
 
-echo '<navbar class="'.$navbar["config"]["navbar-class"].'">';
-foreach ($navbar["items"] as $key) {
-    echo "\n";
-    echo '<a href="'.$app->url->create($key["route"]).'">'.$key["title"].'</a>';
-}
-echo "\n".'</navbar>'."\n";
+?>
+
+<div class="header">
+    <nav class="navbar navbar-inverse bg-inverse rounded navbar-toggleable-md">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#containerNavbar" aria-controls="containerNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="<?= $app->url->create($navbar["items"]["home"]["route"]) ?>">Lösningen</a>
+        <div class="collapse navbar-collapse" id="containerNavbar">
+            <ul class="navbar-nav mr-auto">
+            <?php
+            foreach ($navbar["items"] as $key) {
+                echo '<li class="nav-item">';
+                echo '<a class="nav-link ' . ($app->request->getRoute() == $key["route"] ? "active" : "") . '" href="' . $app->url->create($key["route"]) . '">' . $key["title"] . '</a>';
+                echo '</li>';
+            }
+            ?>
+            </ul>
+        </div>
+    </nav>
+</div>
--- view/site/header.php
+++ view/site/header.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title><?= $title ?></title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
-</head>
-<body>
+<html lang="sv">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?= $title ?></title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <link rel="stylesheet" type="text/css" href="css/narrow-jumbotron.css" >
+    </head>
+    <body>
+
+    <div class="container">
