48cbe97e269c9f5248a9e98382481710a20c31ea
Losningen <schani@live.se>
2017-04-19 14:38:14 +0200

    Added kmom01 report

--- view/site/page/report.php
+++ view/site/page/report.php
@@ -1,7 +1,20 @@
 <h1>Report</h1>
 
 <h2>Kmom01</h2>
-<p>Text.</p>
+<p>
+    <b>Hur känns det att hoppa rakt in i klasser med PHP, gick det bra?</b><br />
+    Tyckte det var ett väldigt stort kursmoment att börja med. Men när man väl kom in i objektorienterade tänket så gick det lite lättare att gå vidare inom alla artiklarna.
+    Göra en navbar och bygga en me-sida kanske skulle ha slagits ihop. För just nu känns det som att man hoppar mellan de olika uppgifterna. Annars så påminner detta mycket om C++, där man har construct och getters och setters.
+</p>
+<p>
+    <b>Berätta om dina reflektioner kring ramverk, anax-lite och din me-sida.</b><br />
+    Det gick väl helt okej att få igång allting me ramverket, det mesta fanns i artikeln så man fick mycket hjälp. Sen att bygga upp en design var lite svårare, jag tänkte inte på att man kunde använda ” $app->url->create(””)” som ett exempel i navbar. Jag valde att använda mig utav CDN versionen utav Bootstrap 4 alpha 6. Inga större bekymmer när man bygger upp det från grunden. Använde ett Boostrap exempel som en bas till en början. Men jag gick tillbaka till dokumentationen och anpassade efter det jag ville ha istället.
+</p>
+<p>
+    <b>Gick det bra att komma igång med MySQL, har du liknande erfarenheter sedan tidigare?</b><br />
+    Jodå det gick bra med MySQL, jag läste inte riktigt och jag råkade göra hela uppgiften under kmom01. Det var en bra uppgift, den går igenom lite med vyer och JOIN.
+    Jag har pysslat med MySQL innan, så detta var inget nytt för mig. Det var en bra repetition för min del. Det var vissa saker som var otydliga men efter lite snack i IRC så fixade det sig.
+</p>
 
 <h2>Kmom02</h2>
 <p>Text.</p>
